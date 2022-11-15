20667fdeb73a9a0d85e9c8b2c5c3b6b407081156
Kisluhin Mihail <89089003390@mail.ru>
2022-11-15 19:29:56 +0400

    MainViewController was added

---
+++ ColorizedApp/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  ColorizedApp
+//
+//  Created by Kisluhin Mihail on 15.11.22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}

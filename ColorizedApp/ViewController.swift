234f91685e5737fd99f8bdf6285461981e9def70
Kisluhin Mihail <89089003390@mail.ru>
2022-11-15 16:33:45 +0400

    funds

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet var redLabel: UILabel!
     @IBOutlet var greenLabel: UILabel!
-    @IBOutlet var bluelabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
     
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
@@ -25,9 +25,50 @@ class ViewController: UIViewController {
      
         colorView.layer.cornerRadius = 15
         
+        setColor()
         
+        setValue(for: redLabel, greenLabel, blueLabel)
     }
 
-
+    
+    @IBAction func rgbSliderAction(_ sender: UISlider) {
+        
+        setColor()
+        
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenLabel.text = string(from: greenSlider)
+        default:
+            blueLabel.text = string(from: blueSlider)
+        }
+    }
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redLabel:
+                redLabel.text = string(from: redSlider)
+            case greenLabel:
+                greenLabel.text = string(from: greenSlider)
+            default:
+                blueLabel.text = string(from: blueSlider)
+            }
+        }
+    }
+   
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    
 }
 

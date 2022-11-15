dfb5c3659cb719dd7ca32adb6b4fe3b2d0ea7f91
Kisluhin Mihail <89089003390@mail.ru>
2022-11-15 15:40:59 +0400

    outlets

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var bluelabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        colorView.layer.cornerRadius = 15
+        
+        
     }
 
 
